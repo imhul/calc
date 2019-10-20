26f1a19c626ad364a903af3f9f276fd42cede9e6
kottkachuk <kot.tkachuk@gmail.com>
2019-10-20 15:11:59 +0300

    result & buttons update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
-﻿const form = document.getElementById('form');
+﻿// Created by Kostya Tkachuk at 20.10.2019
+const form = document.getElementById('form');
 const display = document.getElementById('display');
+const result = document.getElementById('result');
 let firstVal = 0;
 let symbVal = '';
 
@@ -9,7 +11,7 @@ form.addEventListener('click', event => {
     switch(val) {
         case '-': onSymbol(val); break;
         case '+': onSymbol(val); break;
-        case 'X': onSymbol(val); break;
+        case '*': onSymbol(val); break;
         case '/': onSymbol(val); break;
         case '%': onSymbol(val); break;
         case 'C': reset(); break;
@@ -24,6 +26,8 @@ form.addEventListener('click', event => {
         case '8': onNumber(val); break;
         case '9': onNumber(val); break;
         case '0': onNumber(val); break;
+        case '00': onNumber('00'); break;
+        case '000': onNumber('000'); break;
         default: break;
     }
 }, false);
@@ -31,26 +35,36 @@ form.addEventListener('click', event => {
 const onNumber = num => {
     display.focus();
     display.value += num;
+    if (symbVal && firstVal) {
+        result.innerHTML = `${firstVal} ${symbVal} ${display.value}`;
+    } else {
+        result.innerHTML = display.value;
+    }
 }
 
 const onSymbol = sym => {
     firstVal = +display.value;
     display.value = '';
     symbVal = sym;
+    result.innerHTML = `${firstVal} ${sym}`;
 }
 
 const calc = () => {
-    const secondVal = display.value;
+    const secondVal = +display.value;
     switch(symbVal) {
         case '-': display.value = firstVal - secondVal; break;
         case '+': display.value = firstVal + secondVal; break;
-        case 'X': display.value = firstVal * secondVal; break;
+        case '*': display.value = firstVal * secondVal; break;
         case '/': display.value = firstVal / secondVal; break;
         case '%': display.value = (secondVal/100) * firstVal; break;
         default: break;
     }
+    result.innerHTML += ' =';
+    display.focus();
 }
 
 const reset = () => {
     display.value = '';
+    result.innerHTML = '';
+    display.blur();
 }
